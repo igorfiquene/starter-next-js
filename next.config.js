090c57ba35ab09b33f90555fee57496916e78ba3
igorfiquene <igorfpessoa@gmail.com>
2023-03-24 01:43:35 -0300

    feat: init project 🥳

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true
+  }
+}
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig
