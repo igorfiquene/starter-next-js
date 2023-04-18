ff959ad987ec6750488e69912f8088ddc20660b8
igorfiquene <igorfpessoa@gmail.com>
2023-04-18 12:54:15 -0300

    feat: create starter container docker

--- next.config.js
+++ next.config.js
@@ -4,8 +4,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
   compiler: {
     styledComponents: true
+  },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
   }
 }
 
